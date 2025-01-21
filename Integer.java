cdf48ebd97f5767d22b064eca640e953a9369e52
abdullah <firec4309@gmail.com>
2025-01-21 00:09:27 -0800

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integer;
+
+/**
+ *
+ * @author Software-04
+ */
+public class Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    String nama = "abdullah";
+    int usia = 16;
+    
+  System.out.println("abdullah" +  nama);
+   System.out.println("abdullah" =  nama);
+}
